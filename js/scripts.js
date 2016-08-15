aa7a8da20fe04c962143376655055e2c9fb74503
Katy Henning and Kevin Johnston <kevin@>
2016-08-15 16:22:59 -0700

    add an email form so that user can email list to themselves

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
     var item2input = $("input#item2").val();
     var item3input = $("input#item3").val();
     var item4input = $("input#item4").val();
-    var inputs = [item1input, item2input, item3input, item4input];
+    var item5input = $("input#item5").val();
+    var item6input = $("input#item6").val();
+    var item7input = $("input#item7").val();
+    var item8input = $("input#item7").val();
+    var inputs = [item1input, item2input, item3input, item4input, item5input, item6input, item7input, item8input];
 
     var capitalInputs = inputs.map(function(input){
       return input.toUpperCase();
@@ -23,11 +27,11 @@ $(document).ready(function() {
       $('ul').append("<li>" + sortedCapitalInput + "</li>");
     });
     if ($("input#item1") === "") {
-      $("form").show();
+      $("#form1").show();
       $("#results").hide();
     } else {
       $("#results").show();
-      $("form").hide();
+      $("#form1").hide();
     };
 
       });
