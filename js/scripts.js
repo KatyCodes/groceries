8e1ab1fa5822b1471d79dd3cd2a75ba81e52b4d8
Katy Henning and Kevin Johnston <katy+kevin@>
2016-08-15 15:20:03 -0700

    Add js code to capitalize and sort inputs

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,23 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
+    var item1input = $("input#item1").val();
+    var item2input = $("input#item2").val();
+    var item3input = $("input#item3").val();
+    var item4input = $("input#item4").val();
+    var inputs = [item1input, item2input, item3input, item4input];
+
+    var capitalInputs = inputs.map(function(input){
+      return input.toUpperCase();
+    });
 
 
-  }
+     console.log(capitalInputs);
+    //
+    // var sortedCapitalInputs = capitalInputs.sort();
+    //     console.log(sortedCapitalInputs);
+      });
 
-});
+    $("#results").submit()
+    $("#results").show();
+  });
